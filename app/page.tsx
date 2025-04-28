0430992ec2692f14878227c3fc749af4b526196c
Md Motiur Rahman <cse.motiur@gmail.com>
2025-04-29 00:48:49 +0100

    external font added and configured

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-
-
 export default function Home() {
   return (
-    <h1 className="text-light-500">Hello</h1>
+    <>
+      <h1 className="text-light-500 h1-bold font-space-grotesk">Whereas disregard and contempt for human rights have resulted</h1>
+      <h1 className="h1-bold ">Whereas disregard and contempt for human rights have resulted</h1>
+    </>
   );
 }
