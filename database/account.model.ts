cdaa81f10f6a1e3b056507032d8ddf0c6b9b19dc
Md Motiur Rahman <cse.motiur@gmail.com>
2025-05-20 02:45:16 +0100

    Account model created

---
+++ database/account.model.ts
@@ -0,0 +1,26 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IAccount {
+  userId: Types.ObjectId;
+  name: string;
+  image?: string;
+  password?: string;
+  provider: string;
+  providerAccountId: string;
+}
+
+const AccountSchema = new Schema<IAccount>(
+  {
+    userId: { type: Schema.Types.ObjectId, ref: "User" },
+    name: { type: String, required: true },
+    image: { type: String, required: false },
+    password: { type: String, required: false },
+    provider: { type: String, required: true },
+    providerAccountId: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Account = models?.account || model<IAccount>("Account", AccountSchema);
+export default Account;
