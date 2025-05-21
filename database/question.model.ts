3f0d4a1d0faaab11220131a9161f02cfcdb577f2
Md Motiur Rahman <cse.motiur@gmail.com>
2025-05-21 01:46:42 +0100

    Question Model created

---
+++ database/question.model.ts
@@ -0,0 +1,32 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  title: string;
+  content: string;
+  tags: Types.ObjectId[];
+  author: Types.ObjectId;
+  views: number;
+  upvotes: number;
+  downvotes: number;
+  answers: number;
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    views: { type: Number, default: 0 },
+    upvotes: { type: Number, default: 0 },
+    downvotes: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Question =
+  models?.question || model<IQuestion>("Question", QuestionSchema);
+export default Question;
